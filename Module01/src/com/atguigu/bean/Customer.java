af8c99611b2022a390c65dfed0e6fa0680924c0f
longshixian <diyutianxie@126.com>
2019-07-01 21:30:51 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,12 @@
+package com.atguigu.bean;
+
+//这是单行注释
+public class Customer {
+    public static void main(String[] args){
+        System.out.println("helloworld!!!!");
+    }
+    public void method(){
+
+    }
+
+}
---
+++ src/com/atguigu/java/Hello.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+/**
+ * @author ADMIN
+ * @date 2019/6/29 - 12:23
+ */
+public class Hello {
+}
